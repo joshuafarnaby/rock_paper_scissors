453c747e83f60095462ef14810c7c707f686e535
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-11-29 08:37:23 +0000

    add function to get computer choice

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ function getPlayerChoice() {
 
   while (keepAsking) {
     input = prompt('Player! Choose your weapon!', 'Rock, Paper, Scissors');
-    input = input.trim().toLowerCase()
+    input = input.trim().toLowerCase();
 
     if (input == 'rock' || input == 'paper' || input == 'scissors') {
       keepAsking = false;
     } else {
-      alert('Player, that is not a valid input! Try again.')
+      alert('Player, that is not a valid input! Try again.');
     }
   }
 
   return input
+}
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber == 1) {
+    return 'rock';
+  } else if (randomNumber == 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 }
